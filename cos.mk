938de7b40f5f17da2469bdd149050950bba04b90
Wizard0f0s <jamie.gilmer@gmail.com>
2013-11-19 15:53:47 -0500

    d2vzw: Initial stage for 4.4 and CoS

--- cos.mk
+++ cos.mk
@@ -1,13 +1,16 @@
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/cdma.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
+
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
 
-PRODUCT_NAME := cm_d2vzw
+PRODUCT_NAME := cos_d2vzw
 PRODUCT_DEVICE := d2vzw
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_d2vzw-eng
+add_lunch_combo cos_d2vzw-eng
 
